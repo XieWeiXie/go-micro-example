b085f7e43d43d321ca28541ea3119a94e8d1bf89
XieWei <wuxiaoshen@shu.edu.cn>
2019-02-20 16:16:11 +0800

    feat: gopkg.in for go project

---
+++ usage/usage.go
@@ -0,0 +1,13 @@
+package usage
+
+import (
+	"fmt"
+
+	"gopkg.in/wuxiaoxiaoshen/go-version.v1/common"
+)
+
+func NewHello() {
+	var hello common.Hello
+	fmt.Println(hello.Print())
+	fmt.Println(hello.Println("Python"))
+}
